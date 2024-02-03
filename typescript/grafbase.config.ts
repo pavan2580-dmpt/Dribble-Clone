af355d87945b7e2eb3719109449fb8d3dd600236
pavan2580-dmpt <pavanganesh2580@gmail.com>
2024-02-03 19:50:30 +0530

    config the graf/sdk

--- typescript/grafbase.config.ts
+++ typescript/grafbase.config.ts
@@ -1,68 +1,35 @@
-import { graph, config } from '@grafbase/sdk'
+import { graph, config } from '@grafbase/sdk';
 
-// Welcome to Grafbase!
-//
-// Configure authentication, data sources, resolvers and caching for your GraphQL API.
-
-
-const g = graph.Standalone()
-
-
+const g = graph.Standalone();
 
 const User = g.model('User', {
-  name: g.string(),
-  email: g.string().unique(),
-  avatarurl: g.url(),
-  description: g.string().optional(),
-  githuburl: g.url().optional(),
-  linkedinurl: g.url().optional(),
-  projects: g.relation(()=>Projects).list(),
+  name : g.string(),
+  email:g.string().unique(),
+  avatarUrl :g.url(),
+  description :g.string().optional(),
+  githubUrl : g.url().optional(),
+  linkedinUrl :g.url().optional(),
+  projects:g.relation(()=>Project.list().optional()),
 });
 
-const Projects = g.model('Project',{
-  title : g.string(),
+
+const Project = g.model('Project',{
+  title:g.string(),
   description : g.string(),
   image:g.url(),
-  liveSite : g.url(),
-  githubUrl : g.url(),
-  category:g.string().search(),
-  createdBy : g.relation(()=>User),
+  liveSiteUrl : g.url(),
+  githunUrl : g.url(),
+  category : g.string().search(),
+  createdBy:g.relation(()=>User)
 })
 
 
-
-
-// Data Sources - https://grafbase.com/docs/connectors
-//
-// const pg = connector.Postgres('pg', { url: g.env('DATABASE_URL') })
-// g.datasource(pg)
-
-// Resolvers - https://grafbase.com/docs/resolvers
-//
-// g.query('helloWorld', {
-//   returns: g.string(),
-//   resolver: 'hello-world',
-// })
 
 export default config({
   graph: g,
-  // Authentication - https://grafbase.com/docs/auth
   auth: {
-    // OpenID Connect
-    // const oidc = auth.OpenIDConnect({ issuer: g.env('OIDC_ISSUER_URL') })
-    // providers: [oidc],
     rules: (rules) => {
-      rules.public()
+      rules.public();
     },
   },
-  // Caching - https://grafbase.com/docs/graphql-edge-caching
-  // cache: {
-  //   rules: [
-  //     {
-  //       types: ['Query'], // Cache everything for 60 seconds
-  //       maxAge: 60,
-  //       staleWhileRevalidate: 60
-  //     }
-  //   ]
-  // }
-})
+});
