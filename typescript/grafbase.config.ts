a2c82e7bf4371457eec2f032c34270d7e1453a63
pavan2580-dmpt <pavanganesh2580@gmail.com>
2024-02-03 19:11:28 +0530

    first commit

---
+++ typescript/grafbase.config.ts
@@ -0,0 +1,68 @@
+import { graph, config } from '@grafbase/sdk'
+
+// Welcome to Grafbase!
+//
+// Configure authentication, data sources, resolvers and caching for your GraphQL API.
+
+
+const g = graph.Standalone()
+
+
+
+const User = g.model('User', {
+  name: g.string(),
+  email: g.string().unique(),
+  avatarurl: g.url(),
+  description: g.string().optional(),
+  githuburl: g.url().optional(),
+  linkedinurl: g.url().optional(),
+  projects: g.relation(()=>Projects).list(),
+});
+
+const Projects = g.model('Project',{
+  title : g.string(),
+  description : g.string(),
+  image:g.url(),
+  liveSite : g.url(),
+  githubUrl : g.url(),
+  category:g.string().search(),
+  createdBy : g.relation(()=>User),
+})
+
+
+
+
+// Data Sources - https://grafbase.com/docs/connectors
+//
+// const pg = connector.Postgres('pg', { url: g.env('DATABASE_URL') })
+// g.datasource(pg)
+
+// Resolvers - https://grafbase.com/docs/resolvers
+//
+// g.query('helloWorld', {
+//   returns: g.string(),
+//   resolver: 'hello-world',
+// })
+
+export default config({
+  graph: g,
+  // Authentication - https://grafbase.com/docs/auth
+  auth: {
+    // OpenID Connect
+    // const oidc = auth.OpenIDConnect({ issuer: g.env('OIDC_ISSUER_URL') })
+    // providers: [oidc],
+    rules: (rules) => {
+      rules.public()
+    },
+  },
+  // Caching - https://grafbase.com/docs/graphql-edge-caching
+  // cache: {
+  //   rules: [
+  //     {
+  //       types: ['Query'], // Cache everything for 60 seconds
+  //       maxAge: 60,
+  //       staleWhileRevalidate: 60
+  //     }
+  //   ]
+  // }
+})
