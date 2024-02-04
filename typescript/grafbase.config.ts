289f99271ba6ed14af11b134fe42f41a43e40d1a
pavan2580-dmpt <pavanganesh2580@gmail.com>
2024-02-04 10:03:40 +0530

    added grafbase schema

---
+++ typescript/grafbase.config.ts
@@ -0,0 +1,34 @@
+import { g, config } from '@grafbase/sdk'
+
+const User = g.model('User', {
+  name : g.string(),
+  email:g.string().unique(),
+  avatarUrl :g.url(),
+  description :g.string().optional(),
+  githubUrl : g.url().optional(),
+  linkedinUrl :g.url().optional(),
+  projects:g.relation(()=>Project).list().optional(),
+});
+
+
+const Project = g.model('Project',{
+  title:g.string(),
+  description : g.string(),
+  image:g.url(),
+  liveSiteUrl : g.url(),
+  githunUrl : g.url(),
+  category : g.string().search(),
+  createdBy:g.relation(()=>User)
+})
+
+
+
+export default config({
+  schema: g,
+  auth: {
+
+    rules: (rules) => {
+      rules.public()
+    },
+  },
+})
